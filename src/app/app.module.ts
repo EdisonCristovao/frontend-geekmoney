0cdb8f3b9517d630983f0b64a1647fd713985dce
ecjunior <ecjunior@email.ciasc.sc.gov.br>
2018-02-16 15:59:40 -0200

    criando components e adicionando admin lte

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { CadastroPessoaComponent } from './component/pessoa/cadastro-pessoa/cadastro-pessoa.component';
+import { CadastroLancamentoComponent } from './component/lancamento/cadastro-lancamento/cadastro-lancamento.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CadastroPessoaComponent,
+    CadastroLancamentoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
